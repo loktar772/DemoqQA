435fb6a69a6e761c037787576b79b1a45bf50556
Dima <anisimov771@gmail.com>
2023-02-03 23:19:08 +0300

    add file

--- tests/test_login_form.py
+++ tests/test_login_form.py
@@ -18,15 +18,15 @@ def test_login_form(browser):
     form_page.gender_radio_1.click_force()
     form_page.user_number.send_keys('9992223344')
     time.sleep(2)
-    # form_page.btn_submit.click_force()
-    # time.sleep(2)
-    form_page.user_email.send_keys(Keys.CONTROL + 'a')
+    form_page.btn_submit.click_force()
     time.sleep(2)
-    form_page.user_email.send_keys(Keys.DELETE)
-
-    # assert form_page.modal_dialog.exist()
+    # form_page.user_email.send_keys(Keys.CONTROL + 'a')
     # time.sleep(2)
-    # form_page.btn_close_modal.click_force()
+    # form_page.user_email.send_keys(Keys.DELETE)
+
+    assert form_page.modal_dialog.exist()
+    time.sleep(2)
+    form_page.btn_close_modal.click_force()
 
 
 
